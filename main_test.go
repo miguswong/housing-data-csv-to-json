4da8fb04f9c32f384be2f81ddd8b2d0f8a67bff1
Migus Wong <migus.wong@gmail.com>
2024-10-11 12:51:27 -0500

    more unit tests

--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestFileExsits(t *testing.T) {
+// Tests to make sure that fileExists function is working
+func TestFileExists(t *testing.T) {
 	fileName := "housesInput.csv"
 	exp := true
 	res := fileExists(fileName)
@@ -10,4 +14,23 @@ func TestFileExsits(t *testing.T) {
 	if res != exp {
 		t.Errorf("Expected %t, got %t instead.", exp, res)
 	}
+}
+
+func TestCreateHousingList(t *testing.T) {
+	testData := [][]string{
+		{"value", "income", "age", "rooms", "bedrooms", "pop", "hh", "Extra Column"},
+		{"1", "1", "1", "1", "1", "1", "1", "18"},
+		{"2", "2", "2", "2", "2", "2", "2"},
+	}
+
+	exp := []housingRecord{
+		{Value: 1, Income: 1, Age: 1, Rooms: 1, Bedrooms: 1, Pop: 1, Hh: 1},
+		{Value: 2, Income: 2, Age: 2, Rooms: 2, Bedrooms: 2, Pop: 2, Hh: 2},
+	}
+
+	res := createHousingList(testData)
+	if !slices.Equal(exp, res) {
+		t.Errorf("Expected:\n %v\n Instead, this was the result:\n%v", exp, res)
+	}
+
 }
